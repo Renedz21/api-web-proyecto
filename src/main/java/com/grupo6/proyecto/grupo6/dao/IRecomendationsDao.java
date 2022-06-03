50ba9085efc61e80f921421ac968673c236221d8
FrankGP01 <malisia10000@gmail.com>
2022-06-02 22:12:38 -0500

    cambio return de la clase a una lista de una clase

--- src/main/java/com/grupo6/proyecto/grupo6/dao/IRecomendationsDao.java
+++ src/main/java/com/grupo6/proyecto/grupo6/dao/IRecomendationsDao.java
@@ -3,8 +3,10 @@ package com.grupo6.proyecto.grupo6.dao;
 import com.grupo6.proyecto.grupo6.entity.Recomendations;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface IRecomendationsDao extends JpaRepository<Recomendations, Long> {
 
-    Recomendations findAllByLevel(Long level);
+    List<Recomendations> findAllByLevel(Long level);
 }
